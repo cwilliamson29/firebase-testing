71602a64fcf504f85678b53e277ec70cc853d73e
Chris Williamson <chriswill0629@gmail.com>
2025-04-02 10:10:54 -0400

    initial commit

---
+++ src/components/auth.tsx
@@ -0,0 +1,11 @@
+
+function Auth() {
+    return (
+        <div>
+            <input placeholder="Email Address" type="email" required/>
+            <input placeholder="Password" type="password" required/>
+        </div>
+    )
+}
+
+export default Auth
